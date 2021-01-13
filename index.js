2e871b20b61459ca7fe140aa456d3a1e0065b00e
Adarsh Agarwal <adarshagarwal8@gmail.com>
2021-01-13 12:20:20 +0530

    Initialised express Router

--- index.js
+++ index.js
@@ -5,3 +5,6 @@ app.get('/', (req, res) => res.send('Hello World with Express'));
 app.listen(port, function () {
      console.log("Running RestHub on port " + port);
 });
+let apiRoutes = require("./api-routes")
+// Use Api routes in the App
+app.use('/api', apiRoutes)
