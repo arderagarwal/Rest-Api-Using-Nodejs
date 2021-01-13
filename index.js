bdafe399ee74bb57f82a1f04eb6fe0e284631f80
Adarsh Agarwal <adarshagarwal8@gmail.com>
2021-01-13 12:14:20 +0530

    Configured the express server

---
+++ index.js
@@ -0,0 +1,7 @@
+let express = require ('express')
+let app = express()
+var port = 8080;
+app.get('/', (req, res) => res.send('Hello World with Express'));
+app.listen(port, function () {
+     console.log("Running RestHub on port " + port);
+});
